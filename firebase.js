17af1e2656950638fe0a24335a127718f91ac5e0
ramya <ramya.rolly@gmail.com>
2020-08-10 08:31:06 -0400

    Initialize project using Create React App

---
+++ firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBl94KsGigmZb-L2f5GPtcnKRroYyuz4Xk",
+    authDomain: "clone-68c61.firebaseapp.com",
+    databaseURL: "https://clone-68c61.firebaseio.com",
+    projectId: "clone-68c61",
+    storageBucket: "clone-68c61.appspot.com",
+    messagingSenderId: "466044330164",
+    appId: "1:466044330164:web:68365225cf836fb0a437e1",
+    measurementId: "G-YQMYCL7L8G"
+  };
